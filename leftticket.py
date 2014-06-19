a47c48b24a5bd188bdd38ddd8f0ce3699a78bad9
Ubuntu <ubuntu@ip-172-31-0-74.us-west-2.compute.internal>
2014-06-19 04:42:57 +0000

    requests

--- leftticket.py
+++ leftticket.py
@@ -1,12 +1,11 @@
 import re
-import urllib2
-import json
+import requests 
 import config
 
 def checkLeftTicket (train_no, date, from_station, to_station):
     proceed = config.LeftTicketConfig(date,from_station,to_station)
     print proceed.url
-    response = urllib2.urlopen(proceed.url)
-    html = json.loads(response.read())
+    response = requests.get(proceed.url)
+    html = response.json()
     train = [train for train in eval(proceed.json_format) if train_no == train[config.train_no]][0]
     return json.dumps(dict((k,train[v]) for (k,v) in config.seats.items() if re.compile(r'\d+').match(train[v])))
--- proceed.py
+++ proceed.py
@@ -1,12 +1,12 @@
-import urllib2
-import json
+import re
+import requests
 import sys
 from config import *
 
 no = "G1013"
 query = LeftTicketConfig("2014-06-20","YDQ","GZQ")
 print query.url
-response = urllib2.urlopen(query.url)
-html = json.loads(response.read())
+response = requests.get(query.url, verify=False)
+html = response.json()
 train = [train for train in eval(query.json_format) if no == train[train_no]][0]
-print train
+print dict((seats[k], train[v]) for (k,v) in seats.items() if re.compile(r'\d+').match(train[v]))
